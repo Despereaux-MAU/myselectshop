d37687ec39cd8bc14124e7f5f733fec041443a74
Despereaux <rlaehdwn025@naver.com>
2024-10-26 14:46:23 +0900

    회원기능 구현

---
+++ src/main/java/com/despereaux/myselectshop/repository/UserRepository.java
@@ -0,0 +1,11 @@
+package com.despereaux.myselectshop.repository;
+
+import com.despereaux.myselectshop.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByUsername(String username);
+    Optional<User> findByEmail(String email);
+}
