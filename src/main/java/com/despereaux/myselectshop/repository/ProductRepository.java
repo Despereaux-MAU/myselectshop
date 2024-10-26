2aeabbd3cd9ab4ae0279439795d99ba6a710d978
Despereaux <rlaehdwn025@naver.com>
2024-10-26 20:33:09 +0900

    폴더 별 관심상품 조회 구현

--- src/main/java/com/despereaux/myselectshop/repository/ProductRepository.java
+++ src/main/java/com/despereaux/myselectshop/repository/ProductRepository.java
@@ -1,5 +1,6 @@
 package com.despereaux.myselectshop.repository;
 
+import com.despereaux.myselectshop.dto.ProductResponseDto;
 import com.despereaux.myselectshop.entity.Product;
 import com.despereaux.myselectshop.entity.User;
 import org.springframework.data.domain.Page;
@@ -8,4 +9,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ProductRepository extends JpaRepository<Product, Long> {
     Page<Product> findAllByUser(User user, Pageable pageable);
+
+    Page<Product> findAllByUserAndProductFolderList_FolderId(User user, Long folderId, Pageable pageable);
 }
